3b7e919ee4ef3237beab6348962e7203ab8df113
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-13 01:45:44 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
 def countdown
+  x = 10
+while x < 10
+  puts "#{10 second(s) 9 second(s) 8 second(s) 7 second(s) 6 second(s) 5 second(s) 4 second(s) 3 second(s) 2 second(s) 1 second(s)} "
+  x += 1
 end
+
+it "returns HAPPY NEW YEAR!"
